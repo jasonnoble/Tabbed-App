2d3fa99a2dd4b0b1d971f617bed81904db1dd6dc
Jason Noble <github+jasonn@jasonnoble.org>
2016-02-06 23:18:03 -0700

    HW complete

--- Tabbed-App/FirstViewController.swift
+++ Tabbed-App/FirstViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var temperatureTextField: UITextField!
+    @IBOutlet weak var convertButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var resultsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        resultsLabel.text = ""
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func resetButtonTouched(sender: UIButton) {
+        temperatureTextField.resignFirstResponder()
+        resultsLabel.text = ""
+        temperatureTextField.text = ""
+    }
+    @IBAction func convertButtonTouched(sender: UIButton) {
+        temperatureTextField.resignFirstResponder()
+        if let temperatureInC = Double(temperatureTextField.text!) {
+            let temperatureInF = temperatureInC * 9 / 5 + 32
+            resultsLabel.text = "\(temperatureInC) ℃ is \(temperatureInF) ℉"
+        } else {
+            resultsLabel.text = "Please enter a valid decimal number"
+        }
+    }
 
 }
 
--- Tabbed-App/SecondViewController.swift
+++ Tabbed-App/SecondViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var temperatureTextField: UITextField!
+    @IBOutlet weak var convertButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var resultsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        resultsLabel.text = ""
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func resetButtonTouched(sender: UIButton) {
+        temperatureTextField.resignFirstResponder()
+        resultsLabel.text = ""
+        temperatureTextField.text = ""
+    }
+    @IBAction func convertButtonTouched(sender: UIButton) {
+        temperatureTextField.resignFirstResponder()
+        if let temperatureInF = Double(temperatureTextField.text!) {
+            let temperatureInC = (temperatureInF - 32) * 5 / 9
+            resultsLabel.text = "\(temperatureInF) ℉ is \(temperatureInC) ℃"
+        } else {
+            resultsLabel.text = "Please enter a valid decimal number"
+        }
+    }
 
 }
 
